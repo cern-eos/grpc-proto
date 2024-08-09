7ea0d79ec2d0fcce688859dcf6eae9d0328f1d0b
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2024-08-09 12:18:36 +0200

    Removed script belonging to obsoleted Travis CI

--- getprotoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROTOBUF_VERSION=3.0.0
-PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
-pushd /home/travis
-wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
-unzip -o  ${PROTOC_FILENAME}
-bin/protoc --version
-popd
